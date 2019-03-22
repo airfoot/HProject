08a983b1a115ea328ad6deaf103d9a2042a53814
airfoot <wanglei_h@hotmail.com>
2019-03-22 17:33:09 +0800

    initial

---
+++ HProject/Controllers/IT/DistributingArticleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HProject.Controllers.IT
+{
+    public class DistributingArticleController : Controller
+    {
+        // GET: DistributingArticle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
